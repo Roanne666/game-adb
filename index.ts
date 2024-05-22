3daa1697055ac5849b1dc7280c716dee29f2ead4
Roanne <a275313730@qq.com>
2024-05-22 11:15:34 +0800

    🦄 refactor: Move to src

--- index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./src/adb";
-export * from "./src/device";
-export * from "./src/task";
-export * from "./src/types";
-export * from "./src/command";
-export * as utils from "./src/utils";
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export * from "./adb";
+export * from "./device";
+export * from "./task";
+export * from "./types";
+export * from "./command";
+export * as utils from "./utils";
