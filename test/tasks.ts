b96dbddde8b278724d3f7fd21af91c659e9118e0
Roanne <a275313730@qq.com>
2024-05-23 09:25:32 +0800

    test: modify test

--- test/tasks.ts
+++ test/tasks.ts
@@ -2,24 +2,24 @@ import { Task, TapCommand, SwipeCommand, type Device } from "../src";
 
 export const quickMakeTask = new Task({
   name: "quick_make",
-  async handler(device: Device) {
+  async checker(device: Device) {
     await device.getScreencap();
     return true;
   },
   commands: [
     // open popup
-    new TapCommand({ x: 41, y: 640, width: 28, height: 25 }),
+    new TapCommand({ rect: { x: 41, y: 640, width: 28, height: 25 } }),
     // open quick make
-    new TapCommand({ x: 856, y: 94, width: 134, height: 29 }),
+    new TapCommand({ rect: { x: 856, y: 94, width: 134, height: 29 } }),
     // swipe ap
-    new SwipeCommand({ x: 519, y: 361 }, { x: 747, y: 368 }),
+    new SwipeCommand({ originRect: { x: 519, y: 361 }, targetRect: { x: 747, y: 368 } }),
     // confirm quick make
-    new TapCommand({ x: 535, y: 496, width: 102, height: 31 }, { postDelay: 1000 }),
+    new TapCommand({ rect: { x: 535, y: 496, width: 102, height: 31 }, postDelay: 1000 }),
     // confirm reward
-    new TapCommand({ x: 535, y: 496, width: 102, height: 31 }),
-    new TapCommand({ x: 535, y: 496, width: 102, height: 31 }, { postDelay: 1000 }),
+    new TapCommand({ rect: { x: 535, y: 496, width: 102, height: 31 } }),
+    new TapCommand({ rect: { x: 535, y: 496, width: 102, height: 31 }, postDelay: 1000 }),
     // close popup
-    new TapCommand({ x: 1039, y: 87, width: 19, height: 30 }),
+    new TapCommand({ rect: { x: 1039, y: 87, width: 19, height: 30 } }),
   ],
   next: ["save_data"],
 });
@@ -28,12 +28,12 @@ export const saveDataTask = new Task({
   name: "save_data",
   commands: [
     // open popup
-    new TapCommand({ x: 1070, y: 12, width: 20, height: 25 }),
+    new TapCommand({ rect: { x: 1070, y: 12, width: 20, height: 25 } }),
     // click save
-    new TapCommand({ x: 710, y: 262, width: 100, height: 39 }),
+    new TapCommand({ rect: { x: 710, y: 262, width: 100, height: 39 } }),
     // confirm save
-    new TapCommand({ x: 495, y: 420, width: 113, height: 28 }),
+    new TapCommand({ rect: { x: 495, y: 420, width: 113, height: 28 } }),
     // close popup
-    new TapCommand({ x: 958, y: 193, width: 18, height: 25 }),
+    new TapCommand({ rect: { x: 958, y: 193, width: 18, height: 25 } }),
   ],
 });
--- test/test-command.ts
+++ test/test-command.ts
@@ -1,4 +1,41 @@
-import { createAdb, TapCommand, Device } from "../src";
+import { createAdb, TapCommand, Device, CommandLifeCycle } from "../src";
+
+const enterFirstAd = new TapCommand({
+  rect: { x: 789, y: 225, width: 212, height: 75 },
+  postDelay: 1000 * 40,
+  checker(device: Device) {
+    console.log(device.serialNumber);
+    return true;
+  },
+});
+const enterSecondAd = new TapCommand({
+  rect: { x: 793, y: 356, width: 212, height: 75 },
+  postDelay: 1000 * 40,
+});
+const enterThirdAd = new TapCommand({
+  rect: { x: 793, y: 489, width: 212, height: 75 },
+  postDelay: 1000 * 40,
+});
+const exitAd = new TapCommand({ rect: { x: 1170, y: 33, width: 69, height: 28 } });
+const reviceAward = new TapCommand({ rect: { x: 711, y: 415, width: 100, height: 100 } });
+const finishCommand = new TapCommand({ rect: { x: 711, y: 415, width: 100, height: 100 } });
+
+let adCount = 1;
+
+function addCommands(device: Device) {
+  if (adCount <= 10) {
+    device.addCommand(enterFirstAd);
+  } else if (adCount <= 20) {
+    device.addCommand(enterSecondAd);
+  } else if (adCount <= 30) {
+    device.addCommand(enterThirdAd);
+  } else {
+    return;
+  }
+  device.addCommand(exitAd);
+  device.addCommand(reviceAward);
+  device.addCommand(finishCommand);
+}
 
 (async () => {
   const adb = await createAdb(process.argv[2]);
@@ -6,38 +43,14 @@ import { createAdb, TapCommand, Device } from "../src";
   const device = adb.devices.find((d) => d.serialNumber === "emulator-5554");
 
   if (device) {
-    let adCount = 1;
-    const enterFirstAd = new TapCommand({ x: 789, y: 225, width: 212, height: 75 }, { postDelay: 1000 * 40 });
-    const enterSecondAd = new TapCommand({ x: 793, y: 356, width: 212, height: 75 }, { postDelay: 1000 * 40 });
-    const enterThirdAd = new TapCommand({ x: 793, y: 489, width: 212, height: 75 }, { postDelay: 1000 * 40 });
-
-    const exitAd = new TapCommand({ x: 1170, y: 33, width: 69, height: 28 }, { postDelay: 1000 * 1 });
-    const reviceAward = new TapCommand({ x: 711, y: 415, width: 100, height: 100 }, { postDelay: 1000 * 1 });
-    const finishCommand = new TapCommand({ x: 711, y: 415, width: 100, height: 100 }, { postDelay: 1000 * 1 });
-
     addCommands(device);
 
-    device.on("command_finish", (command) => {
+    device.on(CommandLifeCycle.command_finish, (command) => {
       if (command.id === finishCommand.id) {
         console.log(`Ad finish - current count ${adCount}`);
         adCount++;
         addCommands(device);
       }
     });
-
-    function addCommands(device: Device) {
-      if (adCount <= 10) {
-        device.addCommand(enterFirstAd);
-      } else if (adCount <= 20) {
-        device.addCommand(enterSecondAd);
-      } else if (adCount <= 30) {
-        device.addCommand(enterThirdAd);
-      } else {
-        return;
-      }
-      device.addCommand(exitAd);
-      device.addCommand(reviceAward);
-      device.addCommand(finishCommand);
-    }
   }
 })();
