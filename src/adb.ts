21ad81f5ab387c770d38886f64cb22761cb6048d
Roanne <a275313730@qq.com>
2024-05-30 10:03:07 +0800

    refactor: remove 'createAdb' and moify 'initDevices'

--- src/adb.ts
+++ src/adb.ts
@@ -1,44 +1,35 @@
 import { Device } from "./device";
 import { issueCommand } from "./utils";
 
-export async function createAdb(adbPath: string) {
-  const adb = new Adb(adbPath);
-  await adb.initDevices();
-  return adb;
-}
-
 export class Adb {
   /**
    * The folder where adb.exe is located
    */
   public readonly path: string;
 
-  /**
-   * The list of devices
-   */
-  public devices: Device[] = [];
-
   constructor(adbPath: string) {
     this.path = adbPath;
   }
 
-  public async initDevices(retry = 3) {
+  public async getDevices(retry = 3) {
     if (retry === 0) throw "Adb get devices failed";
-    const devicesStdout = await issueCommand(this.path, ["devices"]);
+    const devicesStdout = await issueCommand({ adbPath: this.path, args: ["devices"] });
     const deviceInfoArray = devicesStdout.trim().split("\r\n");
     let devicesAttached = false;
+    let devices: Device[] = [];
     for (const info of deviceInfoArray) {
       if (devicesAttached) {
         const [serialNumber, connectStatus] = info.split("\t");
         const newDevice = new Device(this.path, serialNumber, connectStatus === "device" ? true : false);
         newDevice.resolution = await newDevice.getResolution();
-        this.devices.push(newDevice);
+        devices.push(newDevice);
       } else if (info === "List of devices attached") {
         devicesAttached = true;
       }
     }
-    if (this.devices.length === 0) {
-      await this.initDevices(retry - 1);
+    if (devices.length === 0) {
+      devices = await this.getDevices(retry - 1);
     }
+    return devices;
   }
 }
