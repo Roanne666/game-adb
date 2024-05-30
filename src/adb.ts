5168648f4ed9ce64c3160c0aa1e488ce3a075051
Roanne <a275313730@qq.com>
2024-05-30 16:52:10 +0800

    refactor

--- src/adb.ts
+++ src/adb.ts
@@ -1,5 +1,5 @@
 import { Device } from "./device";
-import { issueCommand } from "./utils";
+import { delay, issueCommand } from "./utils";
 
 export class Adb {
   /**
@@ -12,23 +12,25 @@ export class Adb {
   }
 
   public async getDevices(retry = 3) {
-    if (retry === 0) throw "Adb get devices failed";
-    const devicesStdout = await issueCommand({ adbPath: this.path, args: ["devices"] });
-    const deviceInfoArray = devicesStdout.trim().split("\r\n");
-    let devicesAttached = false;
-    let devices: Device[] = [];
-    for (const info of deviceInfoArray) {
-      if (devicesAttached) {
-        const [serialNumber, connectStatus] = info.split("\t");
-        const newDevice = new Device(this.path, serialNumber, connectStatus === "device" ? true : false);
-        newDevice.resolution = await newDevice.getResolution();
-        devices.push(newDevice);
-      } else if (info === "List of devices attached") {
-        devicesAttached = true;
+    const devices: Device[] = [];
+    for (let i = 0; i < retry; i++) {
+      const devicesStdout = await issueCommand({ adbPath: this.path, args: ["devices"] });
+      const deviceInfoArray = devicesStdout.trim().split("\r\n");
+      let devicesAttached = false;
+      for (const info of deviceInfoArray) {
+        if (devicesAttached) {
+          const [serialNumber, connectStatus] = info.split("\t");
+          const newDevice = new Device(this.path, serialNumber, connectStatus === "device");
+          newDevice.deviceResolution = await newDevice.getResolution();
+          devices.push(newDevice);
+        } else if (info === "List of devices attached") {
+          devicesAttached = true;
+        }
       }
-    }
-    if (devices.length === 0) {
-      devices = await this.getDevices(retry - 1);
+
+      if (devices.length > 0) return devices;
+
+      if (i < retry - 1) await delay(3000);
     }
     return devices;
   }
